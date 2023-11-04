d2d4f3ac765373549bee9979d0f05308dfadf326
한소희 <131546223+HanSo-Hee@users.noreply.github.com>
2023-11-04 19:45:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://eeshabot:eeshabot@cluster0.tlau5cc.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://weare:weare@cluster0.7bqh2qr.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "eesha")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'FILES')
 
